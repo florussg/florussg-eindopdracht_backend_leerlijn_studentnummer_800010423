6096662c314ba99dba1e039f9e03ec751d911e89
florussg <glennnewzealand@hotmail.com>
2021-11-11 12:39:03 +0100

    second commit

---
+++ src/main/java/nl/florussg/eindopdracht_novi_backend_800010423/EindopdrachtNoviBackend800010423Application.java
@@ -0,0 +1,13 @@
+package nl.florussg.eindopdracht_novi_backend_800010423;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EindopdrachtNoviBackend800010423Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EindopdrachtNoviBackend800010423Application.class, args);
+    }
+
+}
---
+++ src/test/java/nl/florussg/eindopdracht_novi_backend_800010423/EindopdrachtNoviBackend800010423ApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.florussg.eindopdracht_novi_backend_800010423;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EindopdrachtNoviBackend800010423ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
