7a5fc9c8b58db5c4cb0cd93df5f5bfcb58a74d14
florussg <glennnewzealand@hotmail.com>
2022-02-14 11:39:24 +0100

    push

--- src/main/java/nl/florussg/eindopdracht_novi_backend_800010423/Dto/CustomerDto.java
+++ src/main/java/nl/florussg/eindopdracht_novi_backend_800010423/Dto/CustomerDto.java
@@ -6,15 +6,63 @@ import javax.validation.constraints.Size;
 
 public class CustomerDto {
 
+    //attributen
     private long id;
 
+    @NotBlank
+    @Size(min=2) //The firstname has to be a minimal of 2 characters
     private String firstname;
 
+    @NotBlank
+    @Size(min=2) //The lastname has to be a minimal of 2 characters
     private String lastname;
 
-    private int bsnnumber;
-
+    @NotNull
+    @Size(min=10, max=10) //Validator on dutch phonenumbers only
     private int phonenumber;
 
+    @NotBlank
+    @Size(min=9, max=9)
+    private int bsnnumber;
 
+    //getters and setters
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public int getBsnnumber() {
+        return bsnnumber;
+    }
+
+    public void setBsnnumber(int bsnnumber) {
+        this.bsnnumber = bsnnumber;
+    }
+
+    public int getPhonenumber() {
+        return phonenumber;
+    }
+
+    public void setPhonenumber(int phonenumber) {
+        this.phonenumber = phonenumber;
+    }
 }
--- src/main/java/nl/florussg/eindopdracht_novi_backend_800010423/Services/CustomerService.java
+++ src/main/java/nl/florussg/eindopdracht_novi_backend_800010423/Services/CustomerService.java
@@ -1,9 +1,16 @@
 package nl.florussg.eindopdracht_novi_backend_800010423.Services;
 
+import nl.florussg.eindopdracht_novi_backend_800010423.Dto.CustomerDto;
+import nl.florussg.eindopdracht_novi_backend_800010423.Exceptions.BadRequestException;
+import nl.florussg.eindopdracht_novi_backend_800010423.Exceptions.RecordNotFoundException;
 import nl.florussg.eindopdracht_novi_backend_800010423.Models.Customer;
 import nl.florussg.eindopdracht_novi_backend_800010423.Repositories.CustomerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class CustomerService {
@@ -19,7 +26,34 @@ public class CustomerService {
         }
     }
 
+    public Customer getOneCustomerById (long id) {
+        Optional<Customer> customer = customerRepository.findById(id);
+        if(customer.isPresent()) {
+            return customer.get();
+        } else {
+            throw new RecordNotFoundException("Customer with this id is not found");
+        }
+      }
 
+      public Customer getCustomerByBsnnumber (int bsnnumber) {
+          Optional<Customer> optionalCustomer = customerRepository.findCustomerByBsnnumber(bsnnumber);
+          //Optional<Customer> customer = Optional.ofNullable(customerRepository.findCustomerByBsnnumber(bsnnumber));
+          if (optionalCustomer.isPresent()) {
+              return optionalCustomer.get();
+          } else {
+              throw new RecordNotFoundException("Customer with this bsnnumber is not found");
+          }
+      }
+
+    public Customer addCustomer (CustomerDto customerDto) {
+        Customer customer = new Customer();
+        customer.setFirstname(customerDto.getFirstname());
+        customer.setLastname(customerDto.getLastname());
+        customer.setBsnnumber(customerDto.getBsnnumber());
+        customer.setPhonenumber(customerDto.getPhonenumber());
+
+        return customerRepository.save(customer);
+    }
 
 
 }
