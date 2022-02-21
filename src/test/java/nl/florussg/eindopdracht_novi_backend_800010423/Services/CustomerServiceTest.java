5668f0d0094d1e2bbc3bc2030671323b1306a4e2
florussg <glennnewzealand@hotmail.com>
2022-02-21 10:29:36 +0100

    testing doesnt work yet, rinus

--- src/test/java/nl/florussg/eindopdracht_novi_backend_800010423/Services/CustomerServiceTest.java
+++ src/test/java/nl/florussg/eindopdracht_novi_backend_800010423/Services/CustomerServiceTest.java
@@ -1,32 +1,79 @@
 package nl.florussg.eindopdracht_novi_backend_800010423.Services;
 
 import nl.florussg.eindopdracht_novi_backend_800010423.Dto.CustomerDto;
+import nl.florussg.eindopdracht_novi_backend_800010423.EindopdrachtNoviBackend800010423Application;
 import nl.florussg.eindopdracht_novi_backend_800010423.Models.Customer;
 import nl.florussg.eindopdracht_novi_backend_800010423.Repositories.CustomerRepository;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import static org.junit.jupiter.api.Assertions.*;
+import org.springframework.test.context.ContextConfiguration;
 
-@SpringBootTest
+import java.sql.Array;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+//@SpringBootTest
+//@ContextConfiguration(classes={EindopdrachtNoviBackend800010423Application.class})
+//@EnableConfigurationProperties
 public class CustomerServiceTest {
 
-    @Autowired
+    @InjectMocks
     private CustomerService customerService;
 
-    @MockBean
+    @Mock
     private CustomerRepository customerRepository;
 
-    @MockBean
+    private Customer oneCustomer;
+    private Customer twoCustomer;
+    private List<Customer> customers;
+
     private CustomerDto customerDto;
+
+    @BeforeEach
+    public void setUp() {
+        oneCustomer.setFirstname("Piet");
+        oneCustomer.setLastname("Janssen");
+        oneCustomer.setBsnnumber(123456789);
+        oneCustomer.setPhonenumber(0611111111);
+
+        twoCustomer.setFirstname("Jan");
+        twoCustomer.setLastname("Derksen");
+        twoCustomer.setBsnnumber(125689234);
+        twoCustomer.setPhonenumber(0622222222);
+
+        customerDto.setFirstname("Piet");
+        customerDto.setLastname("Janssen");
+        customerDto.setBsnnumber(123456789);
+        customerDto.setPhonenumber(0611111111);
+
+        customers.add(oneCustomer);
+        customers.add(twoCustomer);
+
+    }
 
 
 
     @Test
     public void getAllCustomers() {
+        when(customerRepository.findAll()).thenReturn(customers);
 
-    }
+        customerService.getAllCustomers("");
+
+        verify(customerRepository,times(1)).findAll();
+
+            }
 
     @Test
     void getOneCustomerById() {
