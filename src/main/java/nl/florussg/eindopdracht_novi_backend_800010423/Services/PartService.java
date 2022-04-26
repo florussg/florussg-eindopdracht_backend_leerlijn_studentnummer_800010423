48f834a1191aa90445d00d2429dc0991ee7f4709
florussg <glennnewzealand@hotmail.com>
2022-04-26 15:33:34 +0200

    clean up PartService

--- src/main/java/nl/florussg/eindopdracht_novi_backend_800010423/Services/PartService.java
+++ src/main/java/nl/florussg/eindopdracht_novi_backend_800010423/Services/PartService.java
@@ -6,24 +6,21 @@ import nl.florussg.eindopdracht_novi_backend_800010423.Models.Part;
 import nl.florussg.eindopdracht_novi_backend_800010423.Repositories.PartRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 import java.util.List;
 import java.util.Optional;
 
 @Service
 public class PartService {
-    
 
     @Autowired
     private PartRepository partRepository;
 
     public Iterable<Part> getAllParts() {
-
         List<Part> all = partRepository.findAll();
         return all;
     }
 
-    public List<Part> getAllPartsByBrandTypeYear (String userInput) {
+    public List<Part> getAllPartsByBrandTypeYear(String userInput) {
         List<Part> all = partRepository.findPartByBrandTypeYearContainingIgnoreCase(userInput);
 
         if (all.size() > 0) {
@@ -33,8 +30,7 @@ public class PartService {
         }
     }
 
-    public long addNewPart (Part part) {
-
+    public long addNewPart(Part part) {
         Optional<Part> optionalPart = partRepository.findPartByDescription(part.getDescription());
         if (optionalPart.isPresent()) {
             throw new BadRequestException("This part already exists!");
@@ -44,7 +40,7 @@ public class PartService {
         }
     }
 
-    public void deletePart (long id) {
+    public void deletePart(long id) {
         Optional<Part> optionalPart = partRepository.findById(id);
         if (optionalPart.isPresent()) {
             partRepository.deleteById(id);
@@ -53,7 +49,7 @@ public class PartService {
         }
     }
 
-    public Part editPart (long id, Part part) {
+    public Part editPart(long id, Part part) {
         Optional<Part> optionalPart = partRepository.findById(id);
         if(optionalPart.isPresent()) {
             Part partToEdit = optionalPart.get();
@@ -63,21 +59,9 @@ public class PartService {
             partToEdit.setPrice(part.getPrice());
 
             partRepository.save(partToEdit);
-
         } else {
             throw new RecordNotFoundException("There is no part with this id");
         }
         return part;
     }
-
-
-
-
-
-
-
-
-
-
-
 }
