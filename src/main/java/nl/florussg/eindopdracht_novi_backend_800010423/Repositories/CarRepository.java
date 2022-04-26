69db358d488a7c82f7b13042ef7b2aa9dcb3c9bb
florussg <glennnewzealand@hotmail.com>
2022-04-26 15:20:00 +0200

    x

--- src/main/java/nl/florussg/eindopdracht_novi_backend_800010423/Repositories/CarRepository.java
+++ src/main/java/nl/florussg/eindopdracht_novi_backend_800010423/Repositories/CarRepository.java
@@ -8,4 +8,5 @@ import java.util.Optional;
 public interface CarRepository extends JpaRepository<Car, Long> {
 
     Optional<Car> findCarByLicenseplateNumberContainingIgnoreCase(String licenseplateNumber);
+    //
 }
