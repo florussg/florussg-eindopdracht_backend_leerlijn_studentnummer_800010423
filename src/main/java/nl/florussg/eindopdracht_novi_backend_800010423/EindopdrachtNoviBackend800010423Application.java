08b87c75c66abb091cb4eccb33a03d14881f21ed
florussg <glennnewzealand@hotmail.com>
2022-04-26 15:41:01 +0200

    finished project

--- src/main/java/nl/florussg/eindopdracht_novi_backend_800010423/EindopdrachtNoviBackend800010423Application.java
+++ src/main/java/nl/florussg/eindopdracht_novi_backend_800010423/EindopdrachtNoviBackend800010423Application.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class EindopdrachtNoviBackend800010423Application {
-
     public static void main(String[] args) {
         SpringApplication.run(EindopdrachtNoviBackend800010423Application.class, args);
     }
-
 }
