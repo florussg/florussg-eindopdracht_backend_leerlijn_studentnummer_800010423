ceb61f52066594526b605b14fc451e30a51d4372
florussg <glennnewzealand@hotmail.com>
2022-04-26 11:42:29 +0200

    clean up CarRegistrationDocumentControllerTest with 12% line coverage, overall coverage exactly 50%

--- src/test/java/nl/florussg/eindopdracht_novi_backend_800010423/Controllers/CarRegistrationDocumentControllerTest.java
+++ src/test/java/nl/florussg/eindopdracht_novi_backend_800010423/Controllers/CarRegistrationDocumentControllerTest.java
@@ -1,28 +1,19 @@
 package nl.florussg.eindopdracht_novi_backend_800010423.Controllers;
 
-import nl.florussg.eindopdracht_novi_backend_800010423.EindopdrachtNoviBackend800010423Application;
-import nl.florussg.eindopdracht_novi_backend_800010423.Models.Car;
 import nl.florussg.eindopdracht_novi_backend_800010423.Models.CarRegistrationDocument;
 import nl.florussg.eindopdracht_novi_backend_800010423.Services.CarRegistrationDocumentService;
-import nl.florussg.eindopdracht_novi_backend_800010423.Services.CarService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.web.servlet.MockMvc;
 
 import javax.servlet.http.HttpServletRequest;
 import java.util.ArrayList;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
-
 
 
 @ExtendWith({MockitoExtension.class})
@@ -59,16 +50,5 @@ class CarRegistrationDocumentControllerTest {
         verify(carRegistrationDocumentService, times(1)).showAllDownloadableCarRegistrationDocuments();
     }
 
-    @Test
-    void singleUpload() {
-    }
 
-
-    @Test
-    void downloadCarRegistrationDocument() {
-    }
-
-    @Test
-    void setCarToUploadedRegistrationDocument() {
-    }
 }
