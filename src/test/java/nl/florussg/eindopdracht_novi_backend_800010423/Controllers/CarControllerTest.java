876d37b285181aa6aaf19335c397918225f25379
florussg <glennnewzealand@hotmail.com>
2022-04-26 19:59:41 +0200

    clean up CarControllerTest

--- src/test/java/nl/florussg/eindopdracht_novi_backend_800010423/Controllers/CarControllerTest.java
+++ src/test/java/nl/florussg/eindopdracht_novi_backend_800010423/Controllers/CarControllerTest.java
@@ -7,14 +7,12 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.ArrayList;
 import java.util.List;
 
 import static org.mockito.Mockito.*;
-
 
 @ExtendWith({MockitoExtension.class})
 class CarControllerTest {
