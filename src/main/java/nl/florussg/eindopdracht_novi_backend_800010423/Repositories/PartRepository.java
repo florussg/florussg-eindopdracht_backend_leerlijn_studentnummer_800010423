f2170947e85655652773efee1ef1fd4282ac5d1c
florussg <glennnewzealand@hotmail.com>
2022-04-26 15:21:47 +0200

    clean up PartRepository

--- src/main/java/nl/florussg/eindopdracht_novi_backend_800010423/Repositories/PartRepository.java
+++ src/main/java/nl/florussg/eindopdracht_novi_backend_800010423/Repositories/PartRepository.java
@@ -2,8 +2,6 @@ package nl.florussg.eindopdracht_novi_backend_800010423.Repositories;
 
 import nl.florussg.eindopdracht_novi_backend_800010423.Models.Part;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.Optional;
@@ -13,9 +11,4 @@ public interface PartRepository extends JpaRepository<Part, Long> {
     List<Part> findPartByBrandTypeYearContainingIgnoreCase(String brandTypeYear);
 
     Optional<Part> findPartByDescription (String description);
-
-
-
-
-
 }
