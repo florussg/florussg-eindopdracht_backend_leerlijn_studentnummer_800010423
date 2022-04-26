467db70f8e2f8c5c9ccca1a3c30c44cc860f2eed
florussg <glennnewzealand@hotmail.com>
2022-04-26 15:21:21 +0200

    clean up CustomerRepository

--- src/main/java/nl/florussg/eindopdracht_novi_backend_800010423/Repositories/CustomerRepository.java
+++ src/main/java/nl/florussg/eindopdracht_novi_backend_800010423/Repositories/CustomerRepository.java
@@ -1,7 +1,7 @@
 package nl.florussg.eindopdracht_novi_backend_800010423.Repositories;
+
 import nl.florussg.eindopdracht_novi_backend_800010423.Models.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import java.util.Optional;
 
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
@@ -9,5 +9,4 @@ public interface CustomerRepository extends JpaRepository<Customer, Long> {
     Iterable<Customer> findAllByLastnameContainingIgnoreCase(String lastname);
 
     Optional<Customer> findCustomerByBsnnumber(int bsnnumber);
-
 }
