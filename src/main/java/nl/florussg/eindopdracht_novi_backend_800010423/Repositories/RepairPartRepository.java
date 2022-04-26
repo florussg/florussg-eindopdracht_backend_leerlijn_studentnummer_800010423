ccec459bd2e85b7b8270fc103b052892beb1ef69
florussg <glennnewzealand@hotmail.com>
2022-04-26 15:22:16 +0200

    clean up RepairPartRepository

--- src/main/java/nl/florussg/eindopdracht_novi_backend_800010423/Repositories/RepairPartRepository.java
+++ src/main/java/nl/florussg/eindopdracht_novi_backend_800010423/Repositories/RepairPartRepository.java
@@ -2,13 +2,10 @@ package nl.florussg.eindopdracht_novi_backend_800010423.Repositories;
 
 import nl.florussg.eindopdracht_novi_backend_800010423.Models.RepairPart;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 public interface RepairPartRepository extends JpaRepository<RepairPart, Long> {
 
     List<RepairPart> findAllByRepairId(long repairId);
-
-
 }
