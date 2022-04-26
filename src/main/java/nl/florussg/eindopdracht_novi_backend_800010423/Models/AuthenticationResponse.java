25a279975499e5c45f1405e3b6eb125fc7d91711
florussg <glennnewzealand@hotmail.com>
2022-04-26 14:25:29 +0200

    clean up AuthenticationResponse

--- src/main/java/nl/florussg/eindopdracht_novi_backend_800010423/Models/AuthenticationResponse.java
+++ src/main/java/nl/florussg/eindopdracht_novi_backend_800010423/Models/AuthenticationResponse.java
@@ -2,7 +2,6 @@ package nl.florussg.eindopdracht_novi_backend_800010423.Models;
 
 public class AuthenticationResponse {
 
-
     // Response for authentication -> jwt token
     private final String jwt;
 
