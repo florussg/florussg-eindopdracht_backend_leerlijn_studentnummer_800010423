c72ea57b0a864572d1ebcc307915958e359182b8
florussg <glennnewzealand@hotmail.com>
2022-04-26 15:18:44 +0200

    clean up AppointmentRepository

--- src/main/java/nl/florussg/eindopdracht_novi_backend_800010423/Repositories/AppointmentRepository.java
+++ src/main/java/nl/florussg/eindopdracht_novi_backend_800010423/Repositories/AppointmentRepository.java
@@ -4,11 +4,7 @@ import nl.florussg.eindopdracht_novi_backend_800010423.Models.Appointment;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
-
-import java.time.LocalDateTime;
-import java.util.Iterator;
 import java.util.List;
-import java.util.Optional;
 
 public interface AppointmentRepository extends JpaRepository<Appointment, Long> {
 
