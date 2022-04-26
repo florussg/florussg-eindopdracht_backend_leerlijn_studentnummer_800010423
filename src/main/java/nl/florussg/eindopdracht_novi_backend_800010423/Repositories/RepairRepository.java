d236718117181dc2523ba356b1a4b6e27b128e9d
florussg <glennnewzealand@hotmail.com>
2022-04-26 15:22:59 +0200

    clean up RepairRepository

--- src/main/java/nl/florussg/eindopdracht_novi_backend_800010423/Repositories/RepairRepository.java
+++ src/main/java/nl/florussg/eindopdracht_novi_backend_800010423/Repositories/RepairRepository.java
@@ -3,14 +3,9 @@ package nl.florussg.eindopdracht_novi_backend_800010423.Repositories;
 import nl.florussg.eindopdracht_novi_backend_800010423.Models.Repair;
 import nl.florussg.eindopdracht_novi_backend_800010423.Models.RepairStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import java.util.List;
-import java.util.Optional;
 
 public interface RepairRepository extends JpaRepository<Repair, Long> {
 
-    //List<Repair> getAllRepairsByRepairStatusContaining (String repairStatus);
-    //List<Repair> findRepairByRepairStatusContaining (String repairStatus);
     List<Repair> findRepairByRepairStatusEquals (RepairStatus repairStatus);
-
 }
