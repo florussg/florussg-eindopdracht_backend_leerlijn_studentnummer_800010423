e4cc12ab7a168620d72980e6906d64ada6f09bab
florussg <glennnewzealand@hotmail.com>
2022-04-26 15:23:28 +0200

    clean up UserRepository

--- src/main/java/nl/florussg/eindopdracht_novi_backend_800010423/Repositories/UserRepository.java
+++ src/main/java/nl/florussg/eindopdracht_novi_backend_800010423/Repositories/UserRepository.java
@@ -2,14 +2,10 @@ package nl.florussg.eindopdracht_novi_backend_800010423.Repositories;
 
 import nl.florussg.eindopdracht_novi_backend_800010423.Models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
 import java.util.Optional;
 
-//Long of String needed?
 public interface UserRepository extends JpaRepository<User, Long> {
 
     Optional<User> findByUsername(String username);
-
-    //Optional<User> findById(String username);
 }
